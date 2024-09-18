bf4f5ae0f1199fac517f7d1036ab2c9770378d70
Arromal Jamuna Jayan <arromal.jayan@gmail.com>
2024-09-18 13:22:45 +0100

    Added logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
-  logo: <span>Maker Society</span>,
+  logo: <><Image src="/media/logo.png" alt="Maker logo" width="35" height="35" /> <p style={{ marginLeft: "10px", fontWeight: "bold" }}>Maker Society</p></>,
   // project: {
   //   link: "https://github.com/shuding/nextra-docs-template",
   // },
   chat: {
     link: "https://discord.com",
   },
+
   // docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
     text: "Made with ❤️ by Maker Society",
